30fa6e971c44c1367fc53ae84b654e7f945ffc80
Renu <renunikhilp@gmail.com>
2021-02-22 16:22:15 -0800

    challenge 5

--- js-challenges/app.js
+++ js-challenges/app.js
@@ -40,3 +40,29 @@ function sum (arr) {
   return total;
 }
 console.log("The sum of array is: " + sum([5,10]));
+
+//4. Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.- The range function must be called with the first argument less than or equal to the second argument; otherwise, return the string "First argument must be less than second".
+
+function range ( num1, num2) {
+  let arr = [];
+  if (num1 > num2){
+    return "First argument must be less than second";
+  } else {
+    for (let i = num1; i < num2; i ++) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+console.log("The range is: " + range(5,9));
+
+//5. Reverse the string with its characters in uppercase.
+
+function reverse (str) {
+  let reverseStr = "";
+  reverseStr = str.split("").reverse().join("");
+
+  return reverseStr.toUpperCase();
+}
+console.log("The reversed Uppercased string is " +reverse("hello"));
